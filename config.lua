be042f4c0d5e01e5e3b63928ab031505c7a1e325
João Celos <76658833+Joaocelos@users.noreply.github.com>
2023-03-17 13:49:56 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.NPCSpawnLocation = vector3(0.0, 0.0, 0.0) -- Change this to the location where you want the NPC to spawn
+Config.VehicleHash = "adder" -- Change this to the hash value of the vehicle you want to give keys for
